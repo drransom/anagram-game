a1cc15e954db1e8d2e87149e23949f188fe3179e
Elliot Reed <elliotreed2@gmail.com>
2015-08-18 20:32:48 -0700

    write new function for games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,12 @@
 class GamesController < ApplicationController
+  def new
+    conditions = {min_corpus_count: 500, min_length: 6, max_length: 8,
+                  limit: 8, exclude_part_of_speech: [:family_name, :given_name]}
+    words = Wordnik.word.get_random_words(conditions)
+    alpha = /\A[a-z]*\z/i
+    words_arr = words.select do
+      |word| word.word.match(alpha) && !word.vulgar || word.vulgar == 0
+    end.map { | word| word.word }
+    render json: words_arr
+  end
 end
